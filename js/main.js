f1dc6fe2dc3cae5f47375090a03c8f2a425760c7
JooHyuk <juhyuk.kang2013@gmail.com>
2026-04-27 19:43:01 +0900

    docs: 컴시간 API를 NEIS API로 수정

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ const PROJECTS = {
   'our-class-website': {
     type: 'WEB',
     title: '우리반 웹사이트',
-    desc: '우리 반, 우리 학교의 모든 정보가 한곳에! 급식, 시간표, 학사일정을 한눈에 확인할 수 있어요. 컴시간 백엔드 API를 사용해 프론트엔드로 불러오는 방식, 다양한 기능을 지원합니다.',
+    desc: '우리 반, 우리 학교의 모든 정보가 한곳에! 급식, 시간표, 학사일정을 한눈에 확인할 수 있어요. NEIS API를 사용해 프론트엔드로 불러오는 방식, 다양한 기능을 지원합니다.',
     siteUrl: 'https://eungaram-1-2.github.io/eungaram-1-2-integration/#home',
     githubUrl: 'https://github.com/eungaram-1-2/eungaram-1-2-integration',
     mainImage: './img/our-class-website.png',
